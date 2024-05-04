58ec45ed0a8b575b575deb954075bc9932e07d55
jun707 <juntowork@gmail.com>
2024-05-04 09:02:57 -0500

    Added content 2.10

--- lesson_go/snippetbox/cmd/web/main.go
+++ lesson_go/snippetbox/cmd/web/main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
-    "log"
+    "log/slog"
     "net/http"
+    "flag"
+    "os"
 )
 
 func main() {
+    addr := flag.String("addr", ":8000", "Http network-address")
+    flag.Parse()
+
+    logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
     mux := http.NewServeMux()
+    
+    fileServer := http.FileServer(http.Dir("./ui/static/"))
+    
+    mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
     mux.HandleFunc("GET /{$}", home)
     mux.HandleFunc("GET /snippet/view/{id}", snippetView)
     mux.HandleFunc("GET /snippet/create", snippetCreate)
     mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-    log.Print("starting server on :8000")
+    logger.Info("starting server", slog.Any("addr", ":8000"))  
     
-    err := http.ListenAndServe(":8000", mux)
-    log.Fatal(err)
+    err := http.ListenAndServe(*addr, mux)
+    logger.Error(err.Error())
+    os.Exit(1)   
 }
